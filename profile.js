52e2dc9ea3a372f5f6ba0feed11e1faf11fbc85c
Brandon L. Clark <support@themindspot.com>
2015-01-08 22:28:54 -0800

    INTEGRATION BUG FIX

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
-var secure = require('site-secure');
-var auth   = require('site-auth');
-var user   = require('site-user');
+var Secure = require('site-secure');
+var Auth   = require('site-auth');
+var User   = require('site-user');
 
 var merge = function(obj1,obj2){
     var obj3 = {};
@@ -18,13 +18,13 @@ module.exports.create = function(sess, userObj, cb){
 	
 	// Create new authentication profile.
 	credentials = userObj.credentials;
-	auth.create(credentials, function(err, user){
+	Auth.create(credentials, function(err, user){
 		if(err){return cb(err, null);}
 		
 		// Use new Authentication ID to create profile detail.
 		info.id = user.id;
 		info = merge(userObj.detail, userObj.contact);
-		user.create(info, function(err, detail){
+		User.create(info, function(err, detail){
 			if(err){return cb(err, null);}
 			
 			// Register session.
@@ -41,10 +41,10 @@ module.exports.read = function(id, cb){
 	console.log('site-data', id);
 	var profile = {};
 	
-	user.read(id, function(err, detail){
+	User.read(id, function(err, detail){
 	  if(err){return cb(err, null);}
 	  
-	  auth.read(id, function(err, user){
+	  Auth.read(id, function(err, user){
 		  if(err){return cb(err, null);}
 		  
 		  profile = merge(user, detail);
@@ -62,11 +62,11 @@ module.exports.verify = function(sess, credential, cb){
 	delete credentials.remember;
 	
 	// Attempt authentication
-	auth.verify(credential, function(err, user){
+	Auth.verify(credential, function(err, user){
 		if(err){return cb(err, null);}
 		
 		// If verified get account detail.
-		user.read({_id: user.id}, function(err, detail){
+		User.read({_id: user.id}, function(err, detail){
 			if(err){return cb(err, null);}
 			
 			// Register session.
@@ -85,10 +85,10 @@ module.exports.update = function(sess, userObj, cb){
 	console.log('site-data', userObj);
 	var profile = {};
    
-	auth.update(userObj, function(err, user){
+	Auth.update(userObj, function(err, user){
 		if(err){return cb(err, null);}
 		
-		user.update(userObj, function(err, detail){
+		User.update(userObj, function(err, detail){
 			if(err){return cb(err, null);}
 	
 			// Update registered session.
@@ -104,15 +104,15 @@ module.exports.update = function(sess, userObj, cb){
 module.exports.remove = function(sess, id, cb){	
 	console.log('site-data', id);
 	// Deactivate account.
-	auth.remove(id, function(err, user){
+	Auth.remove(id, function(err, user){
 		if(err){return cb(err, null);}
 		
 		// Remove contact information, deactivate detail.
-		user.remove(id, function(err, detail){
+		User.remove(id, function(err, detail){
 			// TODO Ignore error check for now.  Come back when situation response mechanism is in place.
 			
 			// Unregister session.
-			secure.destroy(sess, function(fail, success){
+			Secure.destroy(sess, function(fail, success){
 				if(fail){
 					return cb(fail, null);
 				} else {
