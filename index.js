6f3fbaccf76a15efb801dc908fd5c880b799aaf4
Brandon L. Clark <support@themindspot.com>
2015-01-16 15:46:44 -0800

    Release 0.2.0 for integration test of site-blog package.

--- index.js
+++ index.js
@@ -4,12 +4,17 @@
  * License: MIT
  */
 
-var secure  = require('site-secure');
-var auth    = require('site-auth');
-var user    = require('site-user');
-var profile = require('./profile');
+var secure  = require('site-secure');// General ssl/tls and session functions
+var auth    = require('site-auth');  // General authorization functions
+var user    = require('site-user');  // General user functions
+var profile = require('./profile');  // Data granting and profile management
+var blog    = require('site-blog');  // General blog functions
 
+// User Management
 module.exports.secure  = secure;
 module.exports.user    = user; 
 module.exports.auth    = auth;
 module.exports.profile = profile;
+
+// Site Management
+module.exports.blog    = blog;
