f53110638e3a925f6f06c4594ce6ea56313c3305
Brandon L. Clark <support@themindspot.com>
2015-01-25 12:40:22 -0800

    Added site-projects package and binded to output.

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
  * License: MIT
  */
 
-var secure  = require('site-secure');// General ssl/tls and session functions
-var auth    = require('site-auth');  // General authorization functions
-var user    = require('site-user');  // General user functions
-var profile = require('./profile');  // Data granting and profile management
-var blog    = require('site-blog');  // General blog functions
+var secure  = require('site-secure');   // General ssl/tls and session data functions
+var auth    = require('site-auth');     // General authorization data functions
+var user    = require('site-user');     // General user data functions
+var profile = require('./profile');     // Data granting and profile data management
+var blog    = require('site-blog');     // General blog data functions
+var project = require('site-projects'); // Project Management data functions
 
 // User Management
 module.exports.secure  = secure;
@@ -18,3 +19,6 @@ module.exports.profile = profile;
 
 // Site Management
 module.exports.blog    = blog;
+
+// Project Management
+module.exports.project = project;
