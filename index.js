1982798b855d2ca6d3e496924cc52f0f30ef82cf
Brandon L. Clark <support@themindspot.com>
2015-04-05 21:29:17 -0700

    survey addition

--- index.js
+++ index.js
@@ -5,11 +5,14 @@
  */
 
 var secure  = require('site-secure');   // General ssl/tls and session data functions
+
 var auth    = require('site-auth');     // General authorization data functions
 var user    = require('site-user');     // General user data functions
 var profile = require('./profile');     // Data granting and profile data management
+
 var blog    = require('site-blog');     // General blog data functions
-var project = require('site-projects'); // Project Management data functions
+
+var survey  = require('site-survey');   // Survey Data 
 
 // User Management
 module.exports.secure  = secure;
@@ -20,5 +23,4 @@ module.exports.profile = profile;
 // Site Management
 module.exports.blog    = blog;
 
-// Project Management
-module.exports.project = project;
+module.exports.survey  = survey;
